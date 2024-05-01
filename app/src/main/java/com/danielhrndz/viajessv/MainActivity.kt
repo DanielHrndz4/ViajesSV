3fc94198f38c5536859e8d136f61738861d22ebd
DanielHrndz4 <darwinhrndz12@gmail.com>
2024-05-01 16:39:45 -0600

    add: add loading screen and bottom navbar

--- app/src/main/java/com/danielhrndz/viajessv/MainActivity.kt
+++ app/src/main/java/com/danielhrndz/viajessv/MainActivity.kt
@@ -39,7 +39,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val navController  = rememberNnewavController()
+            val navController  = rememberNavController()
             var isLoading by remember { mutableStateOf(true) }
             var destination by remember { mutableStateOf<String>("") }
 
